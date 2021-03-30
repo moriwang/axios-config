df827342cae496a9602f925033491487c27cb280
zesheng wang <zeiswan@gmail.com>
2021-03-30 17:44:27 +0800

    feat: axios encapsulation

---
+++ src/api/common.js
@@ -0,0 +1,50 @@
+import axios from '../axios'
+import urls from '../api/url'
+import qs from 'qs'
+
+export function get(action, parameter, path = '') {
+	return new Promise((resolve, reject) => {
+		axios({
+			method: 'get',
+			url: urls[action].requestURL(path),
+			params: parameter
+		})
+			.then(res => {
+				let message = urls[action].onSuccess
+				resolve([res, message])
+			})
+			.catch(err => {
+				let message = urls[action].onFail
+				reject(err, message)
+			})
+	})
+}
+
+export function post(type, action, parameter, path = '') {
+	let args = {}
+	if (type === 'json') {
+		args = {
+			data: JSON.stringify(parameter),
+			headers: { 'Content-Type': 'application/json' }
+		}
+	} else if (type === 'qs') {
+		args = {
+			data: qs.stringify(parameter)
+		}
+	}
+	return new Promise((resolve, reject) => {
+		axios({
+			method: 'post',
+			url: urls[action].requestURL(path),
+			...args
+		})
+			.then(res => {
+				let message = urls[action].onSuccess
+				resolve([res, message])
+			})
+			.catch(err => {
+				let message = urls[action].onFail
+				reject([err, message])
+			})
+	})
+}
---
+++ src/axios.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+axios.defaults.timeout = 5000
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = '/api/'
+
+axios.interceptors.request.use(
+	config => {
+		return config
+	},
+	error => {
+		return Promise.reject(error)
+	}
+)
+
+axios.interceptors.response.use(
+	response => {
+		return response
+	},
+	error => {
+		return Promise.reject(error)
+	}
+)
+
+export default axios
